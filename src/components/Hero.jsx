a50ba13f6cf5c797af4a8cf81327decbdd83b174
Zabedfolio <zabedfolio@gmail.com>
2026-05-26 19:16:38 +0600

    added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -135,6 +135,20 @@ export default function Hero() {
               >
                 More About Me
               </Link>
+              <span className="relative flex h-6 w-6 items-center justify-center rounded-full bg-[#ff5f1a]/20 text-[#ff5f1a] transition-transform duration-300 group-hover:translate-x-0.5">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="h-3.5 w-3.5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M2 8a.75.75 0 0 1 .75-.75h8.69L8.22 4.03a.75.75 0 0 1 1.06-1.06l4.5 4.5a.75.75 0 0 1 0 1.06l-4.5 4.5a.75.75 0 1 1-1.06-1.06l3.22-3.22H2.75A.75.75 0 0 1 2 8Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
             </motion.div>
           </motion.div>
 
